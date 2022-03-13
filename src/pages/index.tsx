c1f8b4d8d0298a4cd23ebb715985763235a9e3be
Adarsh <dev.adarshmohan@gmail.com>
2022-03-13 15:35:19 +0530

    [develop]: Added footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,12 +34,12 @@ export default function Home() {
           ))}
         </div>
       </section>
-      <section className="container projects" id="portfolio">
+      <section className="container portfolio" id="portfolio">
         <Title lightHeading="My" darkHeading="Portfolio" />
-        <div className="passions__container">
-          {PASSIONS.map((passion) => (
-            <ProjectCard key={passion.trivialTitle} />
-          ))}
+        <div className="portfolio__container">
+          <div className="coming-soon">
+            <h3 className="coming-soon__text">Coming Soon...</h3>
+          </div>
         </div>
       </section>
       <section className="container contact" id="contact">
@@ -50,6 +50,24 @@ export default function Home() {
           ))}
         </div>
       </section>
+      <footer className="footer">
+        <div className="footer__bg">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+              className="shape-fill"
+            />
+          </svg>
+        </div>
+        <div className="footer__content">
+          <p className="footer__copyright">Adarsh Mohan &copy;2022</p>
+        </div>
+      </footer>
     </main>
   );
 }
